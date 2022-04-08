8b1331c8a87d4945717115cc6067114dbbff91e2
jagos97 <agostini98@gmail.com>
2022-04-07 19:42:02 -0600

    ok this time for real

--- RollPlay/FriendsWindow.xaml.cs
+++ RollPlay/FriendsWindow.xaml.cs
@@ -26,10 +26,23 @@ namespace RollPlay
 
         private static bool popupShown = false;
         public static Border friendSelected = null;
+        private static Button friendSelectedButton = null;
         public FriendsWindow()
         {
             InitializeComponent();
-            MainGrid.Focus();
+            SearchfriendTextBox.Focus(); 
+            for(int i = 0; i < 10; i++)
+            {
+                Button btn = new Button();
+                int j = i + 121;
+                btn.Content = "NewFriend#" + j.ToString();
+                btn.Height = 30;
+                btn.Background = new SolidColorBrush(Colors.White);
+                btn.BorderBrush = new SolidColorBrush(Colors.LightGray);
+                btn.BorderThickness = new Thickness(0,0,0,2);
+                btn.Click += (object sender, RoutedEventArgs e) => { SelectFriend_Click(sender,e); };
+                PossibleFriendsStack.Children.Add(btn);
+            }
         }
 
         private void CharactersCollectionSubMenuSelector_Click(object sender, RoutedEventArgs e)
@@ -88,7 +101,63 @@ namespace RollPlay
             popupShown = false;
             FriendHolder.Children.Remove(friendSelected);
             SubMenuHolder.Children.Clear();
+            if(FriendHolder.Children.Count == 1)
+            {
+                noCharacterMessage.Visibility = Visibility.Visible;
+
+            }
         }
+
+        private void GoToAddFriend_Click(object sender, RoutedEventArgs e)
+        {
+            AddFriendOverlay.Visibility = Visibility.Visible;
+        }
+        private void GoToPendingFriends_Click(object sender, RoutedEventArgs e)
+        {
+            PendingFriendsOverlay.Visibility = Visibility.Visible;
+
+        }
+        private void ExitFriendOverlay_Click(object sender, RoutedEventArgs e)
+        {
+            AddFriendOverlay.Visibility = Visibility.Collapsed;
+            PendingFriendsOverlay.Visibility = Visibility.Collapsed;
+        }
+
+        private void AddFriend_Click(object sender, RoutedEventArgs e)
+        {
+            string content = friendSelectedButton.Content as string;
+            foreach (Button b in PossibleFriendsStack.Children)
+            {
+                b.Background = new SolidColorBrush(Colors.White);
+            }
+            MessageBox.Show("Friend Request sent to " + content);
+
+        }
+        private void AcceptFriend_Click(object sender, RoutedEventArgs e)
+        {
+            NewFriend.Visibility = Visibility.Visible;
+            FriendRequestsStack.Children.Clear();
+        }
+        private void DeclineFriend_Click(object sender, RoutedEventArgs e)
+        {
+            FriendRequestsStack.Children.Clear();
+        }
+        private void ResindFriendRequest_Click(object sender, RoutedEventArgs e)
+        {
+            FriendRequestsSentStack.Children.Clear();
+        }
+
+        private void SelectFriend_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Button b in PossibleFriendsStack.Children)
+            {
+                b.Background = new SolidColorBrush(Colors.White);
+            }
+            Button btn = sender as Button;
+            friendSelectedButton = btn;
+            btn.Background = new SolidColorBrush(Colors.LightBlue);
+        }
+
 
         private void MenuNavBar_Click(object sender, RoutedEventArgs e)
         {
